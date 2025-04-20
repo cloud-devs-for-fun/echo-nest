7cfb037d43ad754b153ffc7dbd69426cbbc8c27d
adp <adriandp.ext@gmail.com>
2025-04-20 22:23:16 +0800

    update: pool adapter

---
+++ src/libs/auth.ts
@@ -0,0 +1,9 @@
+import PostgresAdapter from '@auth/pg-adapter';
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+import pool from './conn';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PostgresAdapter(pool),
+  providers: [Google],
+});
